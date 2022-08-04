e3da8eb379103867abd9ff8457b4c46cd4f7e969
Aidan717 <66300475+Aidan717@users.noreply.github.com>
2022-08-04 15:39:17 +0200

    added muzzle effect, locked cursor
    added muzzle effect to weapon, locked cursor to center and hid it. left click shoots muzzle, escape key unlocks and unhides the cursor

--- Sci-Fi Demo/Assets/Game/Scripts/Player.cs
+++ Sci-Fi Demo/Assets/Game/Scripts/Player.cs
@@ -8,16 +8,42 @@ public class Player : MonoBehaviour
     private float _moveSpeed = 3.5f;
     private float _gravity = 9.81f;
     private CharacterController _controller;
+    [SerializeField]
+    private  GameObject _muzzleFlash;
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;  
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateRaycast();
         CalcuateMovement();
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void CalculateRaycast()
+    {
+        if (Input.GetMouseButton(0)) {
+            _muzzleFlash.SetActive(true);
+            Ray origin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(origin, out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.transform.name);
+            }
+        } else {
+            _muzzleFlash.SetActive(false);
+        }
     }
 
     void CalcuateMovement() {
